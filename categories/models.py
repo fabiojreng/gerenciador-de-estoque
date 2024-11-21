128bcdb125ad385e3d5f0d44ed9de4de36a9289c
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-11-21 18:33:43 -0300

    feat: renomear nome da class

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Categorie(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
