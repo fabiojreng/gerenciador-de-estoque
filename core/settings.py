28b86580123f82273a353bfe797802482ee1528a
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-11-22 14:26:53 -0300

    feat: definir templates e componentes

--- core/settings.py
+++ core/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["core/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
