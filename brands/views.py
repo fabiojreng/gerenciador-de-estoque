602177adf73b0d61b338c1f7712b5e732e662fa6
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-11-22 14:27:58 -0300

    feat: criar view de listagem e criação de brand

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Brand
+from .forms import BrandForm
 
-# Create your views here.
+
+class BrandListView(ListView):
+    model = Brand
+    template_name = "brand_list.html"
+    context_object_name = "brands"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get("name")
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
+
+
+class BrandCreateView(CreateView):
+    model = Brand
+    template_name = "brand_create.html"
+    form_class = BrandForm
+    success_url = reverse_lazy("brand_list")
