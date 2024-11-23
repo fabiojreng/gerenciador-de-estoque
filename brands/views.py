52866ccd4e03762264cd42b1d6b586847c4d6021
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-11-23 13:52:04 -0300

    feat: criar detalhes da marca por id

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 from django.urls import reverse_lazy
 from .models import Brand
 from .forms import BrandForm
@@ -24,3 +24,9 @@ class BrandCreateView(CreateView):
     template_name = "brand_create.html"
     form_class = BrandForm
     success_url = reverse_lazy("brand_list")
+
+
+class BrandDetailView(DetailView):
+    model = Brand
+    template_name = "brand_detail.html"
+    # context_object_name = "brand"
