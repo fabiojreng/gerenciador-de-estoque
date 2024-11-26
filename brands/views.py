599b57c6fd314174635641ab968f4fae96e642c4
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-11-25 22:07:39 -0300

    feat: adicionar paginação

--- brands/views.py
+++ brands/views.py
@@ -8,13 +8,13 @@ from django.views.generic import (
 from django.urls import reverse_lazy
 from .models import Brand
 from .forms import BrandForm
-from django.contrib import messages
 
 
 class BrandListView(ListView):
     model = Brand
     template_name = "brand_list.html"
     context_object_name = "brands"
+    paginate_by = 3
 
     def get_queryset(self):
         queryset = super().get_queryset()
